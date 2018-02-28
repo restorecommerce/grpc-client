c7db1f97a85e60b9111b58d819a1737b91a8dd3c
akumar <arun.kumar@n-fuse.co>
2018-02-28 10:01:51 +0100

    added streaming request and response test.

--- test/grpc_stream_test.ts
+++ test/grpc_stream_test.ts
@@ -71,12 +71,21 @@ async function requestStream(call, callback) {
   });
 }
 
-async function responseStream(call, callback) {
+async function responseStream(call, context) {
   const response = 'pong';
   for (let i = 0; i < 3; i++) {
     call.write(response);
   }
   call.end();
+}
+
+async function biStream(call, context) {
+  call.on('data', async (req) => {
+    call.write({ result: 'pong' });
+  });
+  call.on('end', () => {
+    call.end();
+  });
 }
 
 describe('grpc-streaming tests', () => {
@@ -86,7 +95,10 @@ describe('grpc-streaming tests', () => {
   const instance = grpcClientCfg.client.stream.publisher.instances[0];
   before(async function startServer() {
     server = new grpc.Server();
-    server.addService(stream_proto.Stream.service, { requestStream, responseStream });
+    server.addService(stream_proto.Stream.service, {
+      requestStream,
+      responseStream, biStream
+    });
     server.bind('localhost:50052', grpc.ServerCredentials.createInsecure());
     server.start();
   });
@@ -124,6 +136,26 @@ describe('grpc-streaming tests', () => {
         streamingResponse[i] = await co(call.read((err, data) => { }));
         const data = await new Promise((resolve, reject) => {
           streamingResponse[i]((err, data) => {
+            resolve(data);
+            should.exist(data.result);
+            data.result.should.equal('pong');
+          });
+        });
+      }
+    });
+
+  it('should connect to server and return streaming response for streaming request',
+    async function checkEndpoint() {
+      const biStream = client.makeEndpoint('biStream', instance);
+      let call = await biStream({ value: 'ping' });
+      let result = [];
+      for (let i = 0; i < 3; i += 1) {
+        await call.write({ value: 'ping' });
+      }
+      for (let i = 0; i < 3; i += 1) {
+        result[i] = await co(call.read((err, data) => { }));
+        const data = await new Promise((resolve, reject) => {
+          result[i]((err, data) => {
             resolve(data);
             should.exist(data.result);
             data.result.should.equal('pong');
