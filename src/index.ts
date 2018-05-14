aa4d88f5ef2ffa1c8428f8c0149d818239c2a681
respinha <rui.espinha.ribeiro@n-fuse.co>
2018-05-14 17:21:07 +0200

    Fixed toStruct

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,51 @@ export * from './microservice/client';
 import * as grpc from './microservice/transport/provider/grpc';
 export { grpc };
 export { Client as grpcClient } from './microservice/transport/provider/grpc';
-import { toStruct } from './microservice/client';
-export { toStruct };
+
+import * as _ from 'lodash';
+export function toStruct(obj: any, fromArray = false): any {
+  const decode = (value: any) => {
+    let decodedVal;
+    if (_.isNumber(value)) {
+      decodedVal = { number_value: value };
+    }
+    else if (_.isString(value)) {
+      decodedVal = { string_value: value };
+    }
+    else if (_.isBoolean(value)) {
+      decodedVal = { bool_value: value };
+    }
+    else if (_.isArray(value)) {
+      decodedVal = {
+        list_value: {
+          values: _.map(value, (v) => {
+            return toStruct(v, true);
+          })
+        }
+      };
+    }
+    else if (_.isObject(value)) {
+      decodedVal = { struct_value: toStruct(value) };
+    }
+
+    return decodedVal;
+  };
+
+  let struct;
+  // fromArray flag is true when iterating
+  // objects inside a JSON array
+  if (!fromArray) {
+    struct = {
+      fields: {
+      },
+    };
+    _.forEach(obj, (value, key) => {
+      struct.fields[key] = decode(value);
+    });
+  }
+  else {
+    struct = decode(obj);
+  }
+
+  return struct;
+}
--- src/microservice/client.ts
+++ src/microservice/client.ts
@@ -172,46 +172,6 @@ function generalFactory(method: any, transports: any, logger: any): any {
   };
 }
 
-export function toStruct(obj, fromArray = false) {
-  let struct;
-  // fromArray flag is true when iterating
-  // objects inside a JSON array
-  if (!fromArray) {
-    struct = {
-      fields: {},
-    };
-  }
-  else {
-    struct = {
-      struct_value: { fields: {} },
-    };
-    struct.struct_value.fields = {};
-  }
-
-  const that = this;
-  _.forEach(obj, (value, key) => {
-    let innerStruct;
-    if (!fromArray) {
-      innerStruct = struct.fields;
-    } else {
-      innerStruct = struct.struct_value.fields;
-    }
-
-    if (_.isNumber(value)) {
-      innerStruct[key] = { number_value: value };
-    } else if (_.isString(value)) {
-      innerStruct[key] = { string_value: value };
-    } else if (_.isBoolean(value)) {
-      innerStruct[key] = { bool_value: value };
-    } else if (_.isArray(value)) {
-      innerStruct[key] = { list_value: { values: _.map(value, (v) => { return that.toStruct(v, true); }) } };
-    } else if (_.isObject(value)) {
-      innerStruct[key] = { struct_value: that.toStruct(value) };
-    }
-  });
-  return struct;
-}
-
 /**
  * Microservice client.
  * @class
